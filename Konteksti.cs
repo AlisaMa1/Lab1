db530094967a7b760735e2a12c6e1a673f901e3b
AlisaMa1 <102972968+AlisaMa1@users.noreply.github.com>
2024-06-26 15:23:10 +0200

    Add files via upload

---
+++ Konteksti.cs
@@ -0,0 +1,24 @@
+﻿using OnlineBookStoreApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace OnlineBookStoreApp.Data
+{
+    public class Konteksti : IdentityDbContext
+    {
+        public Konteksti(DbContextOptions<Konteksti> op) : base(op)
+        {
+
+        }
+
+        public DbSet<Kategoria> Kategorite { get; set; }
+        public DbSet<Kopertina> Kopertinat { get; set; }
+        public DbSet<Produkti> Produktet { get; set; }
+        public DbSet<Shporta> Shportat { get; set; }
+        public DbSet<Perdorusi> Perdorusit { get; set; }
+        public DbSet<Kompania> Kompania { get; set; }
+        public DbSet<Porosia> Porosite { get; set; }
+        public DbSet<DetajetEPorosise> DetajetEPorosive { get; set; }
+    }
+}
