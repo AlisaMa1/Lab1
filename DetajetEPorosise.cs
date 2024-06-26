74b6961ad0d24ba88392d2cd4e7f761da79f9bca
AlisaMa1 <102972968+AlisaMa1@users.noreply.github.com>
2024-06-26 15:26:08 +0200

    Add files via upload

---
+++ DetajetEPorosise.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+
+namespace OnlineBookStoreApp.Models
+{
+	public class DetajetEPorosise
+	{
+		public int Id { get; set; }
+		[Required]
+		public int PorosiaId { get; set; }
+		[ForeignKey("PorosiaId")]
+		[ValidateNever]
+		public Porosia Porosia { get; set; }
+
+		[Required]
+		public int ProduktiId { get; set; }
+		[ForeignKey("ProduktiId")]
+		[ValidateNever]
+		public Produkti Produkti { get; set; }
+		public double Sasia { get; set; }
+		public double Cmimi { get; set; }
+	}
+}
---
+++ Kopertina.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineBookStoreApp.Models
+{
+    [Table("Kopertina")]
+    public class Kopertina
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Specifikoni emrin")]
+        public string Emri { get; set; }
+    }
+}
